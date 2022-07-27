ec9d4b804ba608bf353f38bb0961b832466874af
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-07-27 20:51:41 +0530

    added css file.

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
- return <h2> Expense Item </h2>
+    const expenseDate = new Date();
+    const itemDescription = 'Car Insurance';
+    const itemPrice =8000;
+    return (
+        <div className='expense-item'>
+            <div>{expenseDate.toDateString()}</div>
+            <div className='expense-item__description'>
+                <h2>{itemDescription}</h2>
+                <div className='expense-item__price'>{itemPrice}</div>
+            </div>
+
+        </div>
+    )
 }
 
 export default ExpenseItem;
