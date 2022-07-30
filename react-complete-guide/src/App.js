0f3d372d7fd4c0d400dd9140766c406e01f7fcb2
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-07-30 16:12:11 +0530

    dd

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,15 +2,37 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     {
-      title: 'Car Insurance', amount: 800, date1: new Date(),
-      title: 'Bike Insurance', amount: 80, date1: new Date(),
-      title: 'Cycle Insurance', amount: 8, date1: new Date()
+      title: 'Car Insurance', amount: 800, currentDate: new Date()
+    },
+    {
+      title: 'Bike Insurance', amount: 80, currentDate: new Date()
+    },
+    {
+      title: 'Cycle Insurance', amount: 8, currentDate: new Date()
     }
-  ]
+
+  ];
   return (
     <div>
       <h2>Hello World!</h2>
-      <ExpenseItem date1={expenses[0].date1.toDateString()} title={expenses[0].title} amount={expenses[0].amount} ></ExpenseItem>
+      <ExpenseItem
+        currentDate={expenses[0].currentDate.toDateString()}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+      >
+      </ExpenseItem>
+      <ExpenseItem
+        currentDate={expenses[1].currentDate.toDateString()}
+        title={expenses[1].title}
+        amount={expenses[1].amount} 
+        >
+      </ExpenseItem>
+      <ExpenseItem
+        currentDate={expenses[2].currentDate.toDateString()}
+        title={expenses[2].title}
+        amount={expenses[2].amount} 
+        >
+      </ExpenseItem>
     </div>
   );
 }
--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -4,10 +4,11 @@ function ExpenseItem(props) {
 
     return (
         <div className='expense-item'>
-            <div>{props.date1}</div>
+            <div>{props.currentDate}</div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
-                <div className='expense-item__price'>{props.amount}</div>
+                <div className='expense-item__price'>{props.amount}
+                </div>
             </div>
 
         </div>
